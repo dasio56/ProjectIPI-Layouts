06defcaac58a1f6e6c404b58aedc8fba9e42b30a
Victor <victor.carranz@hotmail.com>
2013-10-27 15:28:56 -0600

    funcion para clases
    funcion para asignar caracteristicas por clase en js funciona! como
    ejemplo muestra asigna un ancho de 400px a cualquier etiqueta que posea
    la clase block

--- js/prueba.js
+++ js/prueba.js
@@ -2,11 +2,11 @@
 //ancho de la pantalla =screen.width; 
 //esto hace referecian a la resolucion
 
-window.onload = tamano();
+//window.onload = tamano();
 
 function tamano(){
-//document.getElementById('eldiv').style.width="300px";
-
+document.getElementById('eldiv').style.width="300px";
+alert("sigo funcionando");
 }
 
 
@@ -14,14 +14,19 @@ var allHTMLTags = new Array();
 //funcion para obtener elemento por medio de la clase    
 function getElementByClass(block) {
     // Creamos un array con todas las etiquetas del HTML
+    
     var allHTMLTags=document.getElementsByTagName("*");
     // Las recorremos
     for (i=0; i<allHTMLTags.length; i++) {
         // Y comprobamos que la clase sea la adecuada
-        if (allHTMLTags[i].className==block) {
+       
+        if (allHTMLTags[i].className=="block") {
             // Aqui ejecutamos lo que queramos a los elementos
             // que coincidan con la clase.
-            allHTMLTags[i].style.width='400px';
+            allHTMLTags[i].style.width="400px";
+         
         }
     }
 }
+
+
