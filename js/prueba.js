cd2230bb992ca176d3589fcde4ca259cef70809f
Victor <victor.carranz@hotmail.com>
2013-10-27 14:03:02 -0600

    pequenios cambios
    en el prueba.js va como consultar el ancho y alto de la resolucion del
    usuario y un pequenio metodo que define el ancho de los divs aun falta
    mucho

---
+++ js/prueba.js
@@ -0,0 +1,10 @@
+//para el alto de la pantalla =screen.height;
+//ancho de la pantalla =screen.width; 
+//esto hace referecian a la resolucion
+
+window.onload = tamano();
+
+function tamano(){
+document.getElementById('eldiv').style.width="300px";
+
+}
